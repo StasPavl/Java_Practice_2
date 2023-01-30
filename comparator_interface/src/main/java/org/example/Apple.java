e9ba25547a1f9d29e885903868f3d2312b3aae6c
stanislavpavlovskyi <lichfb@gmail.com>
2023-01-29 23:55:28 -0500

    Apple and Color classes created

---
+++ comparator_interface/src/main/java/org/example/Apple.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Apple {
+    private int weight;
+    private Color color;
+}
---
+++ comparator_interface/src/main/java/org/example/Color.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum Color {
+    GREEN, RED
+}
