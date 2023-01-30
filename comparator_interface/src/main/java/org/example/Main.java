c8812b0173cd861e2a6359af4e8ef6614d470219
stanislavpavlovskyi <lichfb@gmail.com>
2023-01-29 22:07:11 -0500

    Created Main class for Comparator_interface

---
+++ comparator_interface/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+    }
+}
