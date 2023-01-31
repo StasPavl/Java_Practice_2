423c3584a50ade7d17ae3ab2e5589a97709263e5
stanislavpavlovskyi <lichfb@gmail.com>
2023-01-31 18:32:03 -0500

    Learning Stream methods

--- java_stream/src/main/java/org/example/StreamOperations.java
+++ java_stream/src/main/java/org/example/StreamOperations.java
@@ -1,4 +1,44 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamOperations {
+    public static void main(String[] args) {
+
+
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        list.forEach(System.out::println);
+        System.out.println("*******************************");
+
+
+
+        System.out.println("--------FILTER-----------");
+
+        List<Integer> numbers = Arrays.asList(1,2,1,3,3,2,4);
+        numbers.stream()
+                .filter(i->i%2==0)
+                .forEach(System.out::println);
+
+        System.out.println("*******************************");
+
+        System.out.println("--------DISTINCT-----------");
+
+        numbers.stream()
+                .filter(i -> i%2==0)
+                .distinct()
+                .forEach(System.out::println);
+
+        System.out.println("*******************************");
+
+        System.out.println("--------LIMIT-----------");
+
+        numbers.stream()
+                .filter(i -> i % 2 ==0)
+                .distinct()
+                .limit(1)
+                .forEach(System.out::println);
+
+        System.out.println("*******************************");
+    }
 }
