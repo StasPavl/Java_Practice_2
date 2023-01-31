d596fd68fe2e6c0fe743d4a355877bdb02799fa0
stanislavpavlovskyi <lichfb@gmail.com>
2023-01-30 22:11:13 -0500

    Practice with Comparator.comparing method

--- comparator_interface/src/main/java/org/example/AppleTest.java
+++ comparator_interface/src/main/java/org/example/AppleTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+
 public class AppleTest {
     public static void main(String[] args) {
 
@@ -13,8 +15,25 @@ public class AppleTest {
         inventory.add(new Apple(200,Color.GREEN));
         inventory.add(new Apple(50,Color.RED));
 
-        Comparator<Apple> sortApple = Comparator.comparing(Apple::getWeight);
+        Comparator<Apple> sortApple = comparing(Apple::getWeight);
         inventory.sort(sortApple);
+        System.out.println(inventory);
+
+        //----------------------OR-----------------------
+
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+        //-----------------REVERSED ORDER---------------
+
+        inventory.sort(comparing(Apple::getWeight).reversed());
+
+        //-----------------CHAINING--------------------
+
+        inventory
+                .sort(comparing(Apple::getWeight)
+                .reversed()
+                .thenComparing(Apple::getColor));
         System.out.println(inventory);
 
     }
--- comparator_interface/src/main/java/org/example/Main.java
+++ comparator_interface/src/main/java/org/example/Main.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.sort;
+
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(10,0,15,5,20);
         System.out.println(list);
 
-        Collections.sort(list);
+        sort(list);
         System.out.println(list);
 
         Collections.sort(list,new MyComparator());
         System.out.println(list);
 
-        Collections.sort(list,((o1,o2) -> (o1>02) ? -1 : (o1<o2) ? 1 : 0));
+        Collections.sort(list,((o1, o2) -> (o1>02) ? -1 : (o1<o2) ? 1 : 0));
         System.out.println(list);
 
         list.sort((o1,o2)-> o1.compareTo(o2) );
