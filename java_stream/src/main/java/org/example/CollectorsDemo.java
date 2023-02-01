1572c9f0c526b3a226c1d6d334e2bef72ad57600
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-01 16:54:04 -0500

    Stream Collectors studying

--- java_stream/src/main/java/org/example/CollectorsDemo.java
+++ java_stream/src/main/java/org/example/CollectorsDemo.java
@@ -1,8 +1,10 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.example.tasks.dishTask.Dish;
+import org.example.tasks.dishTask.DishData;
+import org.example.tasks.dishTask.Type;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollectorsDemo {
@@ -19,6 +21,90 @@ public class CollectorsDemo {
                 .collect(Collectors.toCollection(ArrayList::new));
 
         System.out.println(numberList);
+
+        Set<Integer> numberSet = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toCollection(HashSet::new));
+
+        System.out.println(numberSet);
+
+        System.out.println("---------TO LIST-----------");
+
+        //toList(); return a Collector interface that gathers the input data into new list
+
+        List<Integer> numberList2 = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(numberList2);
+
+        System.out.println("---------TO SET-----------");
+        //toSET(); return a Collector interface that gathers the input data into new SET
+
+        Set<Integer> numberSet2 = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toSet());
+        System.out.println(numberSet2);
+
+        System.out.println("---------TO MAP-----------");
+
+        //toMap(Function,Function) return Collector Interface that gathers the input data into a new map
+
+        Map<String, Integer> dishMap = DishData.getAll().stream()
+
+               // .collect(Collectors.toMap(x -> x.getName(), y -> y.getCalories()));
+
+                       .collect(Collectors.toMap(Dish::getName,Dish::getCalories));
+
+        System.out.println(dishMap);
+
+        System.out.println("---------SUMMING INT-----------");
+        //summingInt(ToIntFunction) returns a Collector that produce the sum of Integer values func
+
+        Integer sum = DishData.getAll().stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println(sum);
+
+        System.out.println("---------COUNTING-----------");
+        //return a Collector that counts the number of the elements
+
+        Long elementCount = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.counting());
+
+        System.out.println(elementCount); // evan numbers
+
+        System.out.println("---------AVERAGING INT-----------");
+
+        //returns the avarage of the integers passed value
+
+        Double avarageOfCallories = DishData.getAll().stream()
+                .collect(Collectors.averagingInt(Dish::getCalories));
+        System.out.println(avarageOfCallories);
+
+        System.out.println("---------JOINING-----------");
+        //is used to join various elements of a character or string array into a single object
+
+        List<String> courses = Arrays.asList("Java","JS","TS");
+        String str = courses.stream()
+                .collect(Collectors.joining(","));
+        System.out.println(str);
+
+        System.out.println("---------PARTITIONING BY-----------");
+        //is used to partition a stream of objects(or a set of elements) based on a given prdicate.
+
+        Map<Boolean, List<Dish>> isVegeterian = DishData.getAll().stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian));
+        System.out.println(isVegeterian);
+
+
+        System.out.println("---------GROUPING BY-----------");
+        //is used for grouping objects by some properties and storying results in Mao instance
+
+        Map<Type, List<Dish>> grooupingVeggieDish = DishData.getAll().stream()
+                .collect(Collectors.groupingBy(Dish::getType));
+
+        System.out.println(grooupingVeggieDish);
+
 
     }
 }
