d7b5562b4b1ea25846356aa2991415f7fe8fc14c
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 17:53:47 -0500

    Java 10 Updates

--- java-updates-10-11-12-13-14/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
@@ -1,7 +1,35 @@
 package com.cydeo.java10;
 
+import java.util.Arrays;
+
 public class LocalVariableTypeDemo {
     public static void main(String[] args) {
 
+        /**
+         * Only for Local variables
+         * we can not use this variables for constance variables
+         * Local variables MUST be initialize at time of declaration;
+         */
+
+        //Type Inference
+
+        String abc = "Cydeo";
+
+        var str = "Cydeo";
+        var numbers = 5;
+
+
+        /**
+         * compiler understand only as an Integer
+         */
+        byte x = 1;
+        var y = 1;
+
+        var listOfNumbers = Arrays.asList(3,4,5,6);
+
+        var sum = 0;
+        for (var each : listOfNumbers){
+            sum = sum+each;
+        }
     }
 }
