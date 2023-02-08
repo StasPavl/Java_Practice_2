33de6b69684ae7b2b5c5af58ab0d7c189af066e1
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-07 22:28:28 -0500

    Factory Methods

---
+++ java-9-updates/src/main/java/com/cydeo/CompactStringsDemo.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+public class CompactStringsDemo {
+    public static void main(String[] args) {
+
+        String abc = "Cydeo";
+
+        /**
+         * Before Java 9 Java used UTF-16 coder which create 2 bites for each Character
+         * and
+         * After Java 9 jav using new cider LATIN-1 which create 1 bit for each Character
+         */
+    }
+}
