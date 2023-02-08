4fbc5bd6c0eb6bf2acf6e2f5eccc9639c734eb46
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 00:04:56 -0500

    Private Method in Interface

--- java-9-updates/src/main/java/com/cydeo/PrivateMethodInterface.java
+++ java-9-updates/src/main/java/com/cydeo/PrivateMethodInterface.java
@@ -3,17 +3,20 @@ package com.cydeo;
 import java.time.LocalDate;
 
 public interface PrivateMethodInterface {
+    /**
+     * Created specially for removing deduplicate code lines and make code more readable and clean
+     */
 
     boolean isHoliday(LocalDate date);
 
 
     default  boolean isBusinessDay(LocalDate date){
-        validate(date);
+        validate(date);                                //private method
        return !isHoliday(date);
     }
 
     default LocalDate nextDay(LocalDate date) {
-        validate(date);
+        validate(date);                                 //private method
         LocalDate nextDate = date.plusDays(1);
         return isHoliday(nextDate) ? nextDay(nextDate) : nextDate;
     }
