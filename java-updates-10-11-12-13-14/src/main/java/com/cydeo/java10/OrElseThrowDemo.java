d200336c452a03dbdb79d2dbffaeaa1e71240c8a
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 18:03:42 -0500

    Java 10 Updates

---
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java10/OrElseThrowDemo.java
@@ -0,0 +1,25 @@
+package com.cydeo.java10;
+
+import java.util.Optional;
+
+public class OrElseThrowDemo {
+    public static void main(String[] args) {
+
+        /**
+         * Its replacement of .get()
+         * .get() can give you Exception if No Such Element and will stop your code
+         */
+
+        Optional<String> str = Optional.empty();
+        System.out.println(str.get());
+
+        if (str.isPresent()){
+            System.out.println(str.get());
+        }else{
+            //exception handling
+        }
+
+        System.out.println(str.orElseThrow());
+        System.out.println(str.orElseThrow(() -> new RuntimeException()));
+    }
+}
