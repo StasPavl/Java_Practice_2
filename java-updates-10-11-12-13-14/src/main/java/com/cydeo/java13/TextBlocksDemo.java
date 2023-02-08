1b0b81429074588f4007b3db5b070c6f25625fc4
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 18:59:05 -0500

    Java 13 Updates

--- java-updates-10-11-12-13-14/src/main/java/com/cydeo/java13/TextBlocksDemo.java
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java13/TextBlocksDemo.java
@@ -2,8 +2,22 @@ package com.cydeo.java13;
 
 public class TextBlocksDemo {
     public static void main(String[] args) {
+        /**
+         * Text Block using with """ TEXT """
+         * easy to read text inside the code
+         */
 
 
+        var address = "7925 Jones Branch Drive \n " +
+                "McLean, Va 22102";
+
+        var address2  = """
+    7925 Jones Branch Dr
+    McLean, Va 22102
+    """;
+        System.out.println(address);
+
+        System.out.println(address2);
 
     }
 }
