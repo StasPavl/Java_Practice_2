501d1f4f00c73cf5b23635b9ccb67e6b53479541
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 18:06:45 -0500

    Java 10 Updates

---
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java10/CopyOfDemo.java
@@ -0,0 +1,17 @@
+package com.cydeo.java10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CopyOfDemo {
+    public static void main(String[] args) {
+
+        /**
+         * Create Copy of Collection but making it unmodifiable
+         */
+
+        List<Integer> numberList = Arrays.asList(2,4,6,8);
+
+        List<Integer> unmodifiableList = List.copyOf(numberList);
+    }
+}
