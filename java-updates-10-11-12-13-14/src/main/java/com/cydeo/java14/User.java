6996d8ba6d9e051306b5567b95d0b05401928d9e
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 19:27:29 -0500

    Java 14 Updates

--- java-updates-10-11-12-13-14/src/main/java/com/cydeo/java14/User.java
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java14/User.java
@@ -4,6 +4,9 @@ public record User(String name, String address) {
     /**
      * this type is immutable so this type of class doesnt have SETTER
      */
+    public User{
+
+    }
     //All ARG  CONSTR
     //GETTER
     //TO STRING
