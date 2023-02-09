3117dd20f5f063970bf986199db1970b2855c5d5
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-08 19:27:02 -0500

    Java 14 Updates

---
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,12 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+    /**
+     * this type is immutable so this type of class doesnt have SETTER
+     */
+    //All ARG  CONSTR
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASHCODE
+}
---
+++ java-updates-10-11-12-13-14/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,10 @@
+package com.cydeo.java14;
+
+public class UserTest {
+    public static void main(String[] args) {
+
+        User user = new User("Mike", "VA");
+        System.out.println(user.name());
+        System.out.println(user);
+    }
+}
